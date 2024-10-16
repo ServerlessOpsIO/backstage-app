0858e8060d832e43112a5195127ce50182340b81
Tom McLaughlin <tom@serverlessops.io>
2024-10-16 13:27:03 -0400

    Add Github integration

--- src/packages/backend/src/index.ts
+++ src/packages/backend/src/index.ts
@@ -50,4 +50,7 @@ backend.add(import('@backstage/plugin-search-backend-module-pg/alpha'));
 backend.add(import('@backstage/plugin-search-backend-module-catalog/alpha'));
 backend.add(import('@backstage/plugin-search-backend-module-techdocs/alpha'));
 
+
+backend.add(import('@backstage/plugin-catalog-backend-module-github'));
+
 backend.start();
