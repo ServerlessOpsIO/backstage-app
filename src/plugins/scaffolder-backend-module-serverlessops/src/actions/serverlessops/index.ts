7f0bdf4902e75c938e2bcded3071de884382be18
Tom McLaughlin <tom@serverlessops.io>
2024-11-10 17:15:56 -0500

    add @internal/backstage-plugin-scaffolder-backend-module-serverlessops for custom scaffolder actions
    The CLI for creating new scafolder actions was actually broken OOB. Had to adapt change from here and move location of module.ts as well as modify export in src/index.ts

---
+++ src/plugins/scaffolder-backend-module-serverlessops/src/actions/serverlessops/index.ts
@@ -0,0 +1,6 @@
+//import { scaffolderModule } from './module';
+
+/*
+  @deprecated - this way of importing modules will soon be unsupported, and you should use `backend.add(import(...))` instead.
+*/
+export * from './catalog';
---
+++ src/plugins/scaffolder-backend-module-serverlessops/src/index.ts
@@ -0,0 +1,8 @@
+/***/
+/**
+ * The serverlessops module for @backstage/plugin-scaffolder-backend.
+ *
+ * @packageDocumentation
+ */
+
+export { scaffolderModule as default } from './module'
