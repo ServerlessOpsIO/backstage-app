264602d8c1b52cb212fa0e04c3d7dc985ebd0c17
Tom McLaughlin <tom@serverlessops.io>
2024-11-10 17:22:22 -0500

    Fix indent.
    I defauult to 4 but backstage defaults to 2 so reformat these files

--- src/plugins/catalog-backend-module-serverlessops-catalog/src/module.ts
+++ src/plugins/catalog-backend-module-serverlessops-catalog/src/module.ts
@@ -1,53 +1,53 @@
 import {
-  coreServices,
-  createBackendModule,
-  SchedulerServiceTaskRunner
+    coreServices,
+    createBackendModule,
+    SchedulerServiceTaskRunner
 } from '@backstage/backend-plugin-api'
 import { catalogProcessingExtensionPoint } from '@backstage/plugin-catalog-node/alpha'
 import {
-  ServerlessOpsCatalogProvider,
-  ProviderConfig
+    ServerlessOpsCatalogProvider,
+    ProviderConfig
 } from './provider/ServerlessOpsCatalogProvider'
 
 export const catalogModuleServerlessopsCatalog = createBackendModule({
-  pluginId: 'catalog',
-  moduleId: 'serverlessops-catalog',
-  register(reg) {
-    reg.registerInit({
-      deps: {
-        catalog: catalogProcessingExtensionPoint,
-        logger: coreServices.logger,
-        rootConfig: coreServices.rootConfig,
-        scheduler: coreServices.scheduler
-      },
-      async init({ catalog, rootConfig, logger, scheduler }) {
-        logger.info('Initializing ServerlessOps Catalog')
+    pluginId: 'catalog',
+    moduleId: 'serverlessops-catalog',
+    register(reg) {
+        reg.registerInit({
+            deps: {
+                catalog: catalogProcessingExtensionPoint,
+                logger: coreServices.logger,
+                rootConfig: coreServices.rootConfig,
+                scheduler: coreServices.scheduler
+            },
+            async init({ catalog, rootConfig, logger, scheduler }) {
+                logger.info('Initializing ServerlessOps Catalog')
 
-        const config = rootConfig.get('catalog.providers.serverlessops-catalog') as ProviderConfig | undefined
+                const config = rootConfig.get('catalog.providers.serverlessops-catalog') as ProviderConfig | undefined
 
-        // Create a scheduled task runner
-        const schedule = config?.schedule ?
-          config?.schedule
-          : {
-            initialDelay: { seconds: 0 },
-            frequency: { hours: 1 },
-            timeout: { seconds: 60 }
-          }
+                // Create a scheduled task runner
+                const schedule = config?.schedule ?
+                    config?.schedule
+                    : {
+                        initialDelay: { seconds: 0 },
+                        frequency: { hours: 1 },
+                        timeout: { seconds: 60 }
+                    }
 
-        const taskRunner: SchedulerServiceTaskRunner =
-          scheduler.createScheduledTaskRunner(schedule);
+                const taskRunner: SchedulerServiceTaskRunner =
+                    scheduler.createScheduledTaskRunner(schedule);
 
-        // Initialize the ServerlessOps Catalog provider
-        catalog.addEntityProvider(
-          ServerlessOpsCatalogProvider.fromConfig(
-            rootConfig,
-            {
-              logger,
-              taskRunner
-            }
-          )
-        )
-      },
-    })
-  },
+                // Initialize the ServerlessOps Catalog provider
+                catalog.addEntityProvider(
+                    ServerlessOpsCatalogProvider.fromConfig(
+                        rootConfig,
+                        {
+                            logger,
+                            taskRunner
+                        }
+                    )
+                )
+            },
+        })
+    },
 })
