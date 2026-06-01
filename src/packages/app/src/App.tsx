e31cae09a9c71c5d8bad2dc3ec3aab458bc5612e
Tom McLaughlin <tom@serverlessops.io>
2026-06-01 18:21:31 +0200

    bring signin page back.
    local now allows both guest as well as Google where Google is for testing auth setup for prod

--- src/packages/app/src/App.tsx
+++ src/packages/app/src/App.tsx
@@ -1,7 +1,55 @@
 import { createApp } from '@backstage/frontend-defaults';
 import catalogPlugin from '@backstage/plugin-catalog/alpha';
 import { navModule } from './modules/nav';
+import { configApiRef, googleAuthApiRef, useApi } from '@backstage/core-plugin-api';
+import { SignInPageBlueprint } from '@backstage/plugin-app-react';
+import { SignInPage } from '@backstage/core-components';
+import { createFrontendModule } from '@backstage/frontend-plugin-api';
 
+const signInPage = SignInPageBlueprint.make({
+  params: {
+    loader: async () => props => {
+      const configApi = useApi(configApiRef);
+
+      if (configApi.getString('auth.environment') === 'local') {
+        return (
+          <SignInPage
+            {...props}
+            providers={[
+              'guest',
+              {
+                id: 'google',
+                title: 'Google',
+                message: 'Log in with Google',
+                apiRef: googleAuthApiRef,
+              }
+            ]}
+          />
+        );
+      }
+      return (
+        <SignInPage
+          {...props}
+          provider={
+            {
+              id: 'google',
+              title: 'Google',
+              message: 'Log in with Google',
+              apiRef: googleAuthApiRef,
+            }
+          }
+        />
+      );
+    },
+  },
+});
 export default createApp({
-  features: [catalogPlugin, navModule],
+  features: [
+    catalogPlugin,
+    navModule,
+    createFrontendModule({
+      pluginId: 'app',
+      extensions: [signInPage],
+    }),
+  ],
 });
